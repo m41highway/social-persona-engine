8b079b5f829bd5d987c4a8e0a284b95c9dce583f
Anthony Ng <anthony.ng.ags@gmail.com>
2016-12-17 16:21:57 +0800

    Update package.json

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var Twitter = require('twitter');
  
 var client = new Twitter({
-  consumer_key: '', // fill your own
-  consumer_secret: '', // fill your own
-  access_token_key: '', // fill your own
-  access_token_secret: '' // fill your own
+//   consumer_key: '', // fill your own
+//   consumer_secret: '', // fill your own
+//   access_token_key: '', // fill your own
+//   access_token_secret: '' // fill your own
+  consumer_key: 'dvurxEsAY1UNkUAQNQjO1y2f5',
+  consumer_secret: 'SsYdnDWE1MNcleFtItCmniEujGBMArdCwYa5HjtuXNmghaMTeK',
+  access_token_key: '1731032910-jEuzwwZPBxykCYKstvJQHKKdx3RgAjGGN5gcbZR',
+  access_token_secret: '3c1vMwgKcg5VPK1Iv4cIxng3Y1vYnUxA3GhKZSe73nQE1'
 });
  
 var params = {
-    screen_name: 'm41highway',
+    // screen_name: 'm41highway',
+    screen_name: 'sid_travel',
     count: 50
 };
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
@@ -20,7 +25,7 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
     tweets.forEach(function (tweet) {
         console.log(tweet.created_at);
         console.log(tweet.text);
-        console.log(tweet.entities);
+        // console.log(tweet.entities);
 
         if (tweet.entities.media && tweet.entities.media.length > 0) {
             console.log('=== Media ===');
@@ -30,7 +35,7 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
         }
 
         if (tweet.entities.hashtags && tweet.entities.hashtags.length > 0) {
-            console.log('^^^^^^^^^^^^^^^^^^^^^^');
+            console.log('=== hashtags ===');
             tweet.entities.hashtags.forEach(function (h) {
                 console.log('#' + h.text);
                 console.log(h.indices);
